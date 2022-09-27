496f156e1b8a5193a9861d714b0f060d954a9f73
PromzzyKoncepts <okechukwupromise638@gmail.com>
2022-09-27 23:20:43 +0100

    link fetched api to file

--- src/screens/RocketsScreen.js
+++ src/screens/RocketsScreen.js
@@ -1,10 +1,25 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import Navbar from '../components/Navbar';
+import { fetchRockets } from '../Redux/RocketScreen/rocketscreen';
 
 function RocketsScreen() {
+  const { posts, loading } = useSelector(((state) => state.post));
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(fetchRockets());
+  }, [dispatch]);
+
+  if (loading) {
+    return <h2>Loading...</h2>
+  }
   return (
     <div className="App">
       <Navbar />
+      {posts.map((item) => (
+        <h2>{item.id}</h2>
+        // <h1>good</h1>
+      ))}
     </div>
   );
 }
