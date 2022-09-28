11a6a3a65d2ea01128909d86bbeb05e6d8fe7dca
PromzzyKoncepts <okechukwupromise638@gmail.com>
2022-09-28 22:16:18 +0100

    add reducers for reservation btns

--- src/Redux/RocketScreen/rocketscreen.js
+++ src/Redux/RocketScreen/rocketscreen.js
@@ -1,22 +1,47 @@
 /* eslint-disable no-param-reassign */
-import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 
-const FETCH = 'redux/RocketScreen/rocketscreen/FETCH';
-const url = 'https://api.spacexdata.com/v3/rockets';
+const FETCH = "redux/RocketScreen/rocketscreen/FETCH";
+const url = "https://api.spacexdata.com/v3/rockets";
 
-export const fetchRockets = createAsyncThunk(FETCH,
-  async () => fetch(url).then((res) => res.json()));
-const postSlice = createSlice(({
-  name: 'posts',
+export const fetchRockets = createAsyncThunk(FETCH, async () =>
+  fetch(url).then((res) => res.json())
+);
+const postSlice = createSlice({
+  name: "posts",
   initialState: {
     posts: [],
-    loading: 'idle',
+    loading: "idle",
+  },
+  reducers: {
+    reserveRocket: (state, action) => {
+      const posts = state.posts.map((item) => {
+        if (item.id === action.payload) {
+          item = { ...item, reserved: true };
+          return item;
+        }
+        return item;
+      });
+      return { ...state, posts };
+    },
+    leaveRocket: (state, action) => {
+      const posts = state.posts.map((item) => {
+        if (item.id === action.payload) {
+          item = { ...item, reserved: false };
+          return item;
+        }
+        return item;
+      });
+      return { ...state, posts };
+    },
   },
   extraReducers: {
     [fetchRockets.fulfilled]: (state, action) => {
+      console.log(action.payload);
       state.posts = action.payload;
-      state.loading = 'fulfiled';
+      state.loading = "fulfiled";
     },
   },
-}));
+});
+export const { reserveRocket, leaveRocket } = postSlice.actions;
 export default postSlice.reducer;
