1bf8cc3ccd045bdc52890fdad5ac0f243dee03ed
Habib Payenda <habiburahmanpayenda@gmail.com>
2022-09-28 09:32:17 +0430

    Adds connection to api

---
+++ src/Redux/MissionScreen/missionscreen.js
@@ -0,0 +1,36 @@
+/* eslint-disable no-param-reassign */
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
+
+const baseUrl = 'https://api.spacexdata.com/v3/missions';
+
+export const getMissions = createAsyncThunk(
+  'missions/getMissions',
+  async () => {
+    try {
+      const response = axios.get(baseUrl);
+      console.log(response)
+      return response;
+    } catch (error) {
+      console.log(error)
+      return error;
+    }
+  },
+);
+
+const initialState = {
+  missions: [],
+  loading: 'idle',
+};
+
+export const missionsSlice = createSlice({
+  name: 'missions',
+  initialState,
+  extraReducers: (builder) => {
+    builder.addCase(getMissions.fulfilled, (state, action) => {
+      state.missions = action.payload.data;
+    });
+  },
+});
+
+export default missionsSlice.reducer;
--- src/screens/MissionsScreen.js
+++ src/screens/MissionsScreen.js
@@ -1,7 +1,15 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Navbar from '../components/Navbar';
+import { useDispatch, useSelector } from 'react-redux';
+import { getMissions } from '../Redux/MissionScreen/missionscreen';
 
 function MissionsScreen() {
+  const missions = useSelector((state) => state.missions);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getMissions())
+  }, [])
+  console.log(missions);
   return (
     <div className="App">
       <Navbar />
