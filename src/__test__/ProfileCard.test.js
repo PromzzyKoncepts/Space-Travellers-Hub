cca2d83dab319d4e5b9d306455bf9053a252b473
PromzzyKoncepts <okechukwupromise638@gmail.com>
2022-09-29 16:10:27 +0100

    fix linters

--- src/__test__/ProfileCard.test.js
+++ src/__test__/ProfileCard.test.js
@@ -1,14 +1,14 @@
-import React from "react";
-import renderer from "react-test-renderer";
-import { Provider } from "react-redux";
-import store from "../Redux/ConfigureStore";
-import ProfileCard from "../components/ProfileCard";
+import React from 'react';
+import renderer from 'react-test-renderer';
+import { Provider } from 'react-redux';
+import store from '../Redux/ConfigureStore';
+import ProfileCard from '../components/ProfileCard';
 
-it("renders Component correctly", () => {
+it('renders Component correctly', () => {
   const tree = renderer.create(
     <Provider store={store}>
       <ProfileCard />
-    </Provider>
+    </Provider>,
   );
   expect(tree).toMatchSnapshot();
 });
