e1e11985ba717998e41834b769a7e165011d8972
PromzzyKoncepts <okechukwupromise638@gmail.com>
2022-09-29 16:11:03 +0100

    fix linters errors

--- src/__test__/TableRow.test.js
+++ src/__test__/TableRow.test.js
@@ -1,15 +1,14 @@
-import React from "react";
-import renderer from "react-test-renderer";
-import { Provider } from "react-redux";
-import store from "../Redux/ConfigureStore";
-import Rockets from "../components/Rockets";
-import TableRow from "../components/TableRow";
+import React from 'react';
+import renderer from 'react-test-renderer';
+import { Provider } from 'react-redux';
+import store from '../Redux/ConfigureStore';
+import TableRow from '../components/TableRow';
 
-it("renders Component correctly", () => {
+it('renders Component correctly', () => {
   const tree = renderer.create(
     <Provider store={store}>
       <TableRow />
-    </Provider>
+    </Provider>,
   );
   expect(tree).toMatchSnapshot();
 });
