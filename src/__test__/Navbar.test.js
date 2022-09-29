2fc368ffeab72a5d2017494e6ad5026d5aeecd8b
PromzzyKoncepts <okechukwupromise638@gmail.com>
2022-09-29 16:10:19 +0100

    fix linters

--- src/__test__/Navbar.test.js
+++ src/__test__/Navbar.test.js
@@ -1,14 +1,13 @@
-import React from "react";
-import renderer from "react-test-renderer";
-import { MemoryRouter as Router } from "react-router-dom";
-import store from "../Redux/ConfigureStore";
-import Navbar from "../components/Navbar";
+import React from 'react';
+import renderer from 'react-test-renderer';
+import { MemoryRouter as Router } from 'react-router-dom';
+import Navbar from '../components/Navbar';
 
-it("renders Component correctly", () => {
+it('renders Component correctly', () => {
   const tree = renderer.create(
-    <Router store={store}>
+    <Router>
       <Navbar />
-    </Router>
+    </Router>,
   );
   expect(tree).toMatchSnapshot();
 });
